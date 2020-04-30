a037ce6587b86a4e8d9210461d007f48bfd89f6c
Aayush Pal <ap.aayushpal@gmail.com>
2020-04-30 23:43:30 +0530

    Create nextBinary.js

---
+++ nextBinary.js
@@ -0,0 +1,41 @@
+function decBnry(numberToDecode) {
+  let bnr1 = (numberToDecode >>> 0).toString(2);
+  // console.log(bnr1);
+  return bnr1;
+}
+
+function getNumOfOnes(bnrForRead1) {
+  let noOfOnes = 0;
+  for (let i = 0; i < bnrForRead1.length; i++) {
+    if (bnrForRead1.charAt(i) == 1) {
+      noOfOnes++;
+    }
+  }
+  return noOfOnes;
+}
+
+function getResult(binary,numOfOnes) {
+  let _n = n;
+  //_ n= 10
+  //_n = 11
+  while (true) {
+    _n++;
+    let bnr2 = decBnry(_n);
+    let numOfOnes2 = getNumOfOnes(bnr2)
+    // console.log(_n,bnr2,numOfOnes2)
+    if(numOfOnes == numOfOnes2) {
+      return {result: _n ,binary2 : bnr2};
+      break;
+    }
+  }
+  
+}
+
+let n = 180;
+let binary = decBnry(n)
+let ones = getNumOfOnes(binary)
+// console.log(ones)
+let {result,binary2} = getResult(binary,ones)
+console.log(result)
+console.log(`The input was ${n} with binary ${binary}`)
+console.log(`The result is ${result} with binary ${binary2}`)
