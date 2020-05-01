fd31f5746c1f544c08114581883ee9d9d3dd4951
Aayush Pal <ap.aayushpal@gmail.com>
2020-05-01 20:26:13 +0530

    added merge & quick sorting methods

---
+++ dataStructures/merge.py
@@ -0,0 +1,40 @@
+def mergeSort(myList):
+    if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i = 0
+        j = 0
+        
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
+
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+myList = [54,212,93,17,4,55,20]
+mergeSort(myList)
+print(myList)
